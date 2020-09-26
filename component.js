c7885c3d4b7e4b1a5a2ae086c2aabf3663f276f4
Sukhpreet Singh <46858363+pro-panesar@users.noreply.github.com>
2020-09-26 18:05:58 -0400

    Update component.js

--- component.js
+++ component.js
@@ -2,29 +2,16 @@ const { exec } = require('child_process');
 
 /*
 Structure of your command should be :
-node component test <component-name> <component-name> ...
-
-To skip tests
-node component no-test <component-name> <component-name> ...
+node component <component-name> <component-name> ...
 */
 
 let args = process.argv;
 
 args.shift();
 args.shift();
-const tests = args.shift();
 
-if (tests === 'no-test')
-    args = args.map(arg => `ng g c ${arg} --skipTests`);
-else if (tests === 'test')
-    args = args.map(arg => `ng g c ${arg}`);
-else 
-    throw Error('Unexpected Arguments');
-
-let cmd = args[0];
-
-if (args.length !== 1)
-    cmd = args.join(' && ');
+args = args.map(arg => `ng g c routes/${arg} --skipTests`); 
+let = cmd = args.join(' && ');
 
 exec(cmd, (error, stdout, stderr) => {
     if (error)
